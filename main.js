3d72ab7f1408bc816ba43e0a027790237bb9ce38
Dioma <dioma@gmail.com>
2020-01-22 22:48:59 +0200

    icon fixed

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
 $(document).ready(function () {
     $(".btn").click(function () {
         let url = "https://api.chucknorris.io/jokes/random";
-        $.get(url, value => {
-            $(".dev").html("Chuck joke: " + value.value + "<br>" + " ID: " + value.id + "<br>" + " Icon: " + value.icon_url + "<br>" + " Date: " + value.updated_at);
+        $.get(url, (value, date) => {
+            date = new Date().getDate();
+            $(".dev").html(`
+                <div>
+                    Chuck joke: ${value.value}
+                </div>
+                <div>
+                    ID: ${value.id}
+                </div>
+                <div>
+                    Icon: <img src="https://assets.chucknorris.host/img/avatar/chuck-norris.png">
+                </div>
+                <div>
+                    Date: ${value.created_at.getDate()}
+                </div>
+            `);
         });
     });
 });
